f277904c92917bf8f8e475f38c85be9bbd91cdfc
Rudi van Tonder <rudi.vantonder@re-leased.com>
2020-11-19 20:51:55 +1300

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Peoples.Dal.Startup))]
+namespace Peoples.Dal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
