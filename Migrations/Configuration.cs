327a623acfdcb49d93a2d84e586354f0e946c3de
Rudi van Tonder <rudi.vantonder@re-leased.com>
2020-11-19 21:12:20 +1300

    Initial database

---
+++ Migrations/Configuration.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace Peoples.Dal.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<Peoples.Dal.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+    } 
+}
